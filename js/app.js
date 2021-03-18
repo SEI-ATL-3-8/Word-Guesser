38b85db6a4fef8ce553c06409035125ede4d1000
Derek Ensign <derekensign@gmail.com>
2021-03-18 00:18:13 -0500

    guessing game

--- js/app.js
+++ js/app.js
@@ -8,6 +8,12 @@ let word = "";
 let wordContainer = document.querySelector('#guess-word-container');
 let textBox = document.querySelector('#textbox');
 let messages = document.querySelector('#messages');
+let guessLetters = []
+let guessCounter = 0;
+let isCorrect = null
+let wordArray = []
+
+
 
 /* Functions and app logic */
 
@@ -16,9 +22,13 @@ let messages = document.querySelector('#messages');
 // 2. Display the word blanks in the DOM
 const initialize = event => {
     word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
+    wordArray = word.split('')
+    console.log(wordArray)
     console.log('The word is:', word);
     displayWordStatus();
 }
+
+
 
 // Helper function that adds multiple <div>_</div> to DOM
 const displayWordStatus = () => {
@@ -27,17 +37,64 @@ const displayWordStatus = () => {
         wordContainer.removeChild(wordContainer.firstChild);
     }
     for(let i = 0; i < word.length; i++) {
-        let letter = document.createElement('div');
-        letter.textContent = '_'
-        letter.classList.add("letter");
-        wordContainer.appendChild(letter);
+
+            let letter = document.createElement('div');
+            letter.textContent = '_'
+            letter.classList.add("letter");
+            let letterId = ('letter' + i)
+            letter.setAttribute('id', letterId)
+            wordContainer.appendChild(letter);
+
+
+        }
+}
+
+const checkGuess = () => {
+    
+    for(let i=0; i<guessLetters.length; i++) {
+        let isCorrect = word.includes(textBox.value[i])
+        if(isCorrect) {console.log('Your guess is correct')}
+        else {console.log('Incorrect. Try again!')}
+
+        for(let j = 0; j < wordArray.length; j++) {
+
+            if(wordArray[j] === guessLetters[i]) {
+            let letterId = ('letter' + j)
+            document.querySelector(`#${letterId}`).textContent = wordArray[j]
+            }
+        }
     }
+    checkWin(wordArray.length)
+}
+
+const checkWin = (lettersLeft) => {
+    for(i=0; i<wordArray.length; i++){
+        let letterId = ('letter' + i)
+        
+        
+        if(document.querySelector(`#${letterId}`).textContent !== '_'){
+            
+            lettersLeft--
+            
+        }
+        console.log('Letters left' + lettersLeft)
+        }
+        if(lettersLeft === 0){
+            alert("You win!")
+
+    }
+
 }
 
 // On submit event: Guess a letter or guess the whole word
 const guessLetter = event => {
     event.preventDefault();
-    console.log(`You submitted: ${textBox.value}`);
+    console.log(`You submitted: ${textBox.value}`)
+    guessLetters[guessCounter] = textBox.value
+    console.log(guessLetters)
+    guessCounter++
+    checkGuess()
+
 }
 
 // Display a message to the user in the messagebox
