1f5c11ed8cbe44a7632fb68a120b53bcf0d70986
DiddySmooth <avery.mcclead@gmail.com>
2021-03-17 19:01:11 -0500

    grayson-hw

--- js/app.js
+++ js/app.js
@@ -9,15 +9,18 @@ let wordContainer = document.querySelector('#guess-word-container');
 let textBox = document.querySelector('#textbox');
 let messages = document.querySelector('#messages');
 
+let numCorrect = 0;
+
+let guessedLetters = ""
 /* Functions and app logic */
 
 // Initialize the game: 
 // 1. Reset state variables
 // 2. Display the word blanks in the DOM
 const initialize = event => {
-    word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
-    console.log('The word is:', word);
-    displayWordStatus();
+    
+    getData()
+    
 }
 
 // Helper function that adds multiple <div>_</div> to DOM
@@ -38,12 +41,77 @@ const displayWordStatus = () => {
 const guessLetter = event => {
     event.preventDefault();
     console.log(`You submitted: ${textBox.value}`);
+    let guess = textBox.value
+    console.log(guess)
+    let display = document.querySelectorAll('.letter')
+    let correct = false;
+    guessedLetters = guessedLetters + guess + " " 
+
+    if(guess == word)
+        {
+            console.log("Correct")
+            textBox.value = ""
+            correct = true
+            displayMessage("You have won!")
+        }
+    else{
+        for (let i = 0 ;  i <= word.length; i++){
+            if(guess != word.charAt(i)){
+                console.log("Incorrect")
+            }
+            else{
+                
+                display[i].textContent = guess
+                console.log("Correct" + i)
+                numCorrect ++
+                correct = true
+            }
+        }
+        console.log("Here")
+        if(correct == true){
+            displayMessage(guess + " is correct: ")
+        }
+        else{
+            displayMessage(guess + " is incorrect: ")
+        }
+        correct = false
+        textBox.value = ""
+    }
+    if(numCorrect == word.length)
+    {
+        displayMessage("You have won!")
+    }
+    
 }
 
 // Display a message to the user in the messagebox
 const displayMessage = msg => { 
-    /* Your code here! */
+
+    messages.textContent = msg  + "Previous guesses:    " + guessedLetters
+    
 }
+
+
+
+
+
+
+
+getData = async () =>{
+    
+    try {
+        let res = await fetch('https://random-word-api.herokuapp.com/word?number=1');
+        let data = await res.json()
+        console.log(data)
+        word = data[0]
+        console.log('The word is:', word);
+        displayWordStatus()
+        return data
+    } catch (error) {
+        
+    }
+}
+
 
 /* Event Listeners */
 document.addEventListener('DOMContentLoaded', initialize);
