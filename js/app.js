cdb8f436d7831850187467e7d515f0cde31ca026
ConnorCourtney <connorcourtney123@gmail.com>
2021-03-17 22:31:45 -0400

    coco word guesser

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const WORD_LIST = ['producer', 'brainstorm', 'explosion', 'soup', 'feather']
 
 /* Variables and App State */
 let word = "";
+let displayedWord = [];
+let wordArray = [];
 
 /* DOM References */
 let wordContainer = document.querySelector('#guess-word-container');
@@ -17,7 +19,13 @@ let messages = document.querySelector('#messages');
 const initialize = event => {
     word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
     console.log('The word is:', word);
+    for(let i =0; i < word.length; i++) {
+        wordArray.push(word.charAt(i));
+        displayedWord.push('_');
+    }
+    //console.log(wordArray);
     displayWordStatus();
+    
 }
 
 // Helper function that adds multiple <div>_</div> to DOM
@@ -28,7 +36,7 @@ const displayWordStatus = () => {
     }
     for(let i = 0; i < word.length; i++) {
         let letter = document.createElement('div');
-        letter.textContent = '_'
+        letter.textContent = displayedWord[i];
         letter.classList.add("letter");
         wordContainer.appendChild(letter);
     }
@@ -37,12 +45,42 @@ const displayWordStatus = () => {
 // On submit event: Guess a letter or guess the whole word
 const guessLetter = event => {
     event.preventDefault();
-    console.log(`You submitted: ${textBox.value}`);
-}
+    var guess = textBox.value.toLowerCase();
 
+    console.log(`You submitted: ${textBox.value}`);
+    displayMessage("")
+    if (wordArray.includes(guess)) {
+        console.log('correct')
+        displayMessage("Correct")
+        for(let i =0; i < word.length; i++){
+            if(guess == wordArray[i]){
+                displayedWord[i] = guess
+            }
+        }
+        displayWordStatus();
+        if (displayedWord.includes("_") == false){
+            displayMessage("You Win")
+        }
+
+        
+    } 
+    else if(word.localeCompare(guess) == 0){
+        displayMessage("You Win")
+        displayedWord = wordArray;
+        displayWordStatus();
+}
+    
+    else{
+        console.log('inncorrect letter')
+        displayMessage("Inncorrect")
+
+    }
+
+}
 // Display a message to the user in the messagebox
-const displayMessage = msg => { 
+const displayMessage = (msg) => { 
     /* Your code here! */
+    messages.innerHTML = "<p>"+msg+"</p>"
 }
 
 /* Event Listeners */
